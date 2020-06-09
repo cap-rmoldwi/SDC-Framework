37bc345b4c180a132ac591d974df84db93ce3e40
Bhawesh Kumar <bhkumar@cap.org>
2020-06-09 15:55:10 -0500

    Added validation of xml in a restful service for reusing same XSDs for SDC

--- SDC.WebService/Controllers/SdcController.cs
+++ SDC.WebService/Controllers/SdcController.cs
@@ -1,44 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.Extensions.Logging;
+using SDC.Schema;
+using SDC.WebService.Models;
+using SDC.WebService.Services;
 
 namespace SDC.WebService.Controllers
 {
-    [Route("api/sdc")]
+    [Route("[controller]")]
     [ApiController]
     public class SdcController : ControllerBase
     {
-        // GET: api/<Test>
+        private readonly ILogger<SdcController> _logger;
+        private readonly IValidationService _validationService;
+
+        public SdcController(ILogger<SdcController> logger, IValidationService validationService)
+        {
+            _logger = logger;
+            _validationService = validationService;
+        }
+
+        [HttpPost]
+        [Route("templates")]
+        public ResponseModel<FormDesignType> CreateForm([FromBody] FormCreateModel model)
+        {
+            return new ResponseModel<FormDesignType>(null, new StatusInfo(true,
+                new List<string> { "Message one", "message two" }));
+        }
+
+        [HttpGet]
+        [Route("templates/{id?}")]
+        public IEnumerable<string> GetTemplate()
+        {
+            return new string[] { "template info 1", "template info 2" };
+        }
+
+        [HttpPost]
+        [Route("template/validate")]
+        public ResponseModel<bool> ValidateTemplate([FromBody] string xmlFile)
+        {
+            StatusInfo result = _validationService.ValidateXml(xmlFile);
+            return new ResponseModel<bool>(result.Success, result);
+
+        }
+
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<Test>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<Test>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<Test>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<Test>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ SDC.WebService/Services/IValidationService.cs
@@ -0,0 +1,9 @@
+﻿using SDC.WebService.Models;
+
+namespace SDC.WebService.Services
+{
+    public interface IValidationService
+    {
+        StatusInfo ValidateXml(string xmlFile);
+    }
+}
