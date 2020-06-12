9fc9fc6b798333b569cae6cd10908c25797458de
cap-bhkumar <61798260+cap-bhkumar@users.noreply.github.com>
2020-06-11 21:00:40 -0500

    Feature add service (#3)
    * Initial service commit with dummy controllers
    
    * Added validation of xml in a restful service for reusing same XSDs for SDC
    
    * Added test for verifying generated xml from json

---
+++ SDC.WebService/Models/ResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDC.WebService.Models
+{
+    public class ResponseModel<T>
+    {
+        public ResponseModel(T data, StatusInfo status)
+        {
+            Data = data;
+            Status = status;
+        }
+        public T Data { get; set; }
+
+        public StatusInfo Status { get; set; }
+    }
+}
---
+++ SDC.WebService/Models/StatusInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SDC.WebService.Models
+{
+    public class StatusInfo
+    {
+        public StatusInfo()
+        { 
+        }
+
+        public StatusInfo(bool success, List<string> messages)
+        {
+            Success = success;
+            Messages = messages;
+        }
+        public bool Success { get; set; }
+
+        public List<string> Messages { get; set; }
+    }
+}
